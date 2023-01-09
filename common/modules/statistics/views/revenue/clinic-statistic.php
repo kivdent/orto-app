f6faa17396d4b98e652a975369ce5e41817d6609
Vladimir Korchemnyy <kivdent@gmail.com>
2023-01-09 17:47:15 +0700

    Ближайшее свободное время

--- common/modules/statistics/views/revenue/clinic-statistic.php
+++ common/modules/statistics/views/revenue/clinic-statistic.php
@@ -1,31 +1,33 @@
 <?php
 
 
-
 /* @var $this \yii\web\View */
 /* @var $clinicStatistic \common\modules\statistics\models\ClinicStatistic */
+
 /* @var $type string\common\modules\statistics\models\ClinicStatistic */
 
 use common\modules\invoice\widgets\modalTable\InvoiceModalWidget;
 use common\modules\reports\widgets\financialPeriodChooseWidget\FinancialPeriodChooseWidget;
 use yii\helpers\Html;
 
-$this->title=$clinicStatistic->titles[$type];
+$this->title = $clinicStatistic->titles[$type];
 
-$this->params['breadcrumbs'][] = ['label'=>'Выручка','url'=>['revenue/index','financialPeriodId'=>Yii::$app->request->get('financialPeriodId')]];
+$this->params['breadcrumbs'][] = ['label' => 'Выручка', 'url' => ['revenue/index', 'financialPeriodId' => Yii::$app->request->get('financialPeriodId')]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<?= FinancialPeriodChooseWidget::widget(['link' => '/statistics/revenue/material','var' => 'financialPeriodId',])?>
-<?= Html::a('2022',['/statistics/revenue/clinic-statistic','type'=>'getDoctors','year'=>2022])?><br>
-<?=$this->title.": ".call_user_func([$clinicStatistic,$type.'Summary']);?>
+<?= FinancialPeriodChooseWidget::widget(['link' => '/statistics/revenue/material', 'var' => 'financialPeriodId',]) ?>
+<?= Html::a('2022', ['/statistics/revenue/clinic-statistic', 'type' => 'getDoctors', 'year' => 2022]) ?><br>
+<?= $this->title . ": " . call_user_func([$clinicStatistic, $type . 'Summary']); ?>
 
 
 <!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#positions" aria-controls="positions" role="tab" data-toggle="tab">Позиции</a></li>
-    <li role="presentation"><a href="#allDoctors" aria-controls="allDoctors" role="tab" data-toggle="tab">Таблица по врачам</a></li>
-   </ul>
+    <li role="presentation" class="active"><a href="#positions" aria-controls="positions" role="tab" data-toggle="tab">Позиции</a>
+    </li>
+    <li role="presentation"><a href="#allDoctors" aria-controls="allDoctors" role="tab" data-toggle="tab">Таблица по
+            врачам</a></li>
+</ul>
 
 <!-- Tab panes -->
 
@@ -33,24 +35,21 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="tab-content">
     <div role="tabpanel" class="tab-pane active" id="positions">
         <table class="table table-bordered">
-            <?php foreach (call_user_func([$clinicStatistic,$type.'PriceListItems']) as $position):?>
+            <?php foreach (call_user_func([$clinicStatistic, $type . 'PriceListItems']) as $position): ?>
                 <tr>
-                    <td><?=$position['title']?></td>
-                    <td><?=$position['total']?></td>
+                    <td><?= $position['title'] ?></td>
+                    <td><?= $position['total'] ?></td>
                 </tr>
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </table>
     </div>
 
     <!-- Doctors -->
     <div role="tabpanel" class="tab-pane" id="allDoctors">
-<?=\common\widgets\tableWidget\TableWidget::widget([
-        'table' => call_user_func([$clinicStatistic,$type.'PaymentsForTable'])['table'],
-    'labels' =>  call_user_func([$clinicStatistic,$type.'PaymentsForTable'])['labels']])?>
-
+        <?= \common\widgets\tableWidget\TableWidget::widget([
+            'table' => call_user_func([$clinicStatistic, $type . 'PaymentsForTable'])['table'],
+            'labels' => call_user_func([$clinicStatistic, $type . 'PaymentsForTable'])['labels']]) ?>
     </div>
 
 </div>
-
 </div>
-
